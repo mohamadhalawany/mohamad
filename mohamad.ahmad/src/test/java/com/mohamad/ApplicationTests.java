4a92695aa178d5a4ffc90f3aa27092f9756c44e5
mohamedahmed <mohamedahmed@EGYLPT009.upskwt.com>
2019-08-09 02:39:01 +0200

    Unit Test

--- mohamad.ahmad/src/test/java/com/mohamad/ApplicationTests.java
+++ mohamad.ahmad/src/test/java/com/mohamad/ApplicationTests.java
@@ -1,16 +1,48 @@
 package com.mohamad;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
 
+@WebAppConfiguration
+public class ApplicationTests {
+	
+	@Autowired
+	CountryService service ;
+	
+	@Autowired 
+	WebApplicationContext context ;
+	
+	MockMvc mock = MockMvcBuilders.webAppContextSetup(context) .build() ;
+	
 	@Test
-	public void contextLoads() {
+	public void testFindCountry() throws Exception{
+		String url = "/countries/{code}" ;
+		String code = "GBR" ;
+		
+		MvcResult result = mock.perform(MockMvcRequestBuilders.get(url , code).accept(MediaType.APPLICATION_JSON)).andReturn() ;
+		
+		String content = result.getResponse().getContentAsString() ;
+		int status = result.getResponse().getStatus() ;
+		
+		Assert.assertEquals("INVALID_COUNTRY_CODE", 404 , status);
+		Assert.assertTrue("INVALID_COUNTRY_CODE", content.trim().length() == 0);
 	}
 
 }
